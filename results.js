e17b44d5e5c7b09eda1a64b3668405d9e0e0e6ef
Zynorthis-Main-Desktop <Zynorthis@gmail.com>
2020-12-20 15:16:14 -0600

    Started work on results page. Added results js file to handle results page logic.

---
+++ results.js
@@ -0,0 +1,20 @@
+
+/** Gets the needed search criteria from the url prameters. */
+function getURLParms() {
+
+}
+
+/** Populates the front-end template with user data. */
+function populateTemplate() {
+
+}
+
+/** Return to previous page with given error message. */
+function onError(message) {
+    
+}
+
+/** Handles successfully retreiving information. */
+function onSuccess() {
+
+}
